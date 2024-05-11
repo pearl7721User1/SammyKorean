35442bf292a015d2b118bc00af15b71e8e2f71f8
Giwon Seo <pearl7721@naver.com>
2024-05-11 19:34:45 +0900

    replace project with new spm version

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,21 +18,10 @@ public func configure(_ app: Application) async throws {
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
-/*
-    app.databases.use(.postgres(
-      hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-      port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-      username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-      password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-      database: Environment.get("DATABASE_NAME") ?? databaseName
-    ), as: .psql)
-*/
-    
     app.migrations.add(CreateTodo())
 
     app.views.use(.leaf)
 
-    
 
     // register routes
     try routes(app)
